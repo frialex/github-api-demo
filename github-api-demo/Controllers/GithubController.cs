1a30539b1e3931145470e298ee11a5d7d2285576
Frison B Alexander <fri.b.alex@gmail.com>
2018-03-20 16:32:43 -0400

    just basic search

---
+++ github-api-demo/Controllers/GithubController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Octokit;
+
+namespace github_api_demo.Controllers
+{
+    
+
+    public class GithubController : ApiController
+    {
+    
+
+        [Route("github/search")]
+        [HttpGet]
+        public async Task<SearchRepositoryResult> search(string term)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("my-demo-app"));
+
+            var req = new SearchRepositoriesRequest(term);
+
+            var res = await client.Search.SearchRepo(req);
+
+            return res;
+        }
+
+
+    }
+}
--- github-api-demo/Controllers/HomeController.cs
+++ github-api-demo/Controllers/HomeController.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace github_api_demo.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
 
             return View();
         }
